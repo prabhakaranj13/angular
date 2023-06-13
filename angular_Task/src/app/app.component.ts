d3d33b7dc3bce6f77ae253e8a88fdd440816314a
prabhakaranj13 <prabhakaran1352000@gmail.com>
2023-06-13 22:53:54 +0530

    project added

---
+++ angular_Task/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular_Task';
+  headertitie:string[]=["HOME","PRODUCT","COMPANY","CONTACT"]
+  gallerys=["gallery Image","gallery Image","gallery Image","gallery Image"]
+
+}
